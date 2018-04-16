67c0cce385de3c2d4161ff931552711e1445fa56
Sanil Shah <ss4924@columbia.edu>
2018-04-16 02:47:22 -0400

    board

---
+++ board.js
@@ -0,0 +1,41 @@
+class Board {
+	constructor() {
+		this.board_ = ['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']];
+	}
+
+	reset() {
+		this.board_ = ['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']];
+	}
+
+	isEmpty(x, y) {
+		if (x < 0 || x > 2 || y < 0 || y > 2) {
+			return false;
+		}
+		return this.board_[x][y] == '-';
+	}
+
+	isGameOver() {
+		if (this.hasWon('x') || this.hasWon('o')) {
+			return true;
+		}
+		return this.board_.every(row => row.indexOf('-') == -1);
+	}
+
+	hasWon(p) {
+		const winningPositions = [ 
+			[[0, 0], [0, 1], [0, 2]],
+			[[1, 0], [1, 1], [1, 2]],
+			[[2, 0], [2, 1], [2, 2]],
+			[[0, 0], [1, 0], [2, 0]],
+			[[0, 1], [1, 1], [2, 1]],
+			[[0, 2], [1, 2], [2, 2]],
+			[[0, 0], [1, 1], [2, 2]],
+			[[0, 2], [1, 1], [2, 0]]
+		];
+		return winningPositions.some((winningPosition) => {
+			return winningPosition.every((pos) => {
+				this.board_[pos[0]][pos[1]] == p;
+			});
+		});
+	}
+}
