0bf971fa2775a529c1e913a4c19a91c027065bfc
Sanil Shah <sanils@google.com>
2018-04-16 21:14:24 -0400

    adding game

---
+++ game.js
@@ -0,0 +1,55 @@
+class Game {
+	constructor() {
+		this.board_ = new Board();
+		this.players_ = ['x', 'o'];
+		this.currentPlayer_ = 0;
+		this.init_();
+	}
+
+	reset_() {
+		this.board_.reset();
+		this.currentPlayer_ = 0;
+		this.updateStatus_();
+	}
+
+	init_() {
+		const table = document.getElementById('game');
+		const cells = table.getElementsByTagName('td');
+		if (cells.length != 9) {
+			return;
+		}
+		for (let i = 0; i < 9; i++) {
+			const cell = cells[i];
+			cell.onclick = () => {
+				this.attemptMove_(Math.floor(i/3), i%3);
+			}
+		}
+		const reset = document.getElementById('restart');
+		reset.onclick = () => this.reset_();
+	}
+
+	attemptMove_(x, y) {
+		const player = this.players_[this.currentPlayer_]; 
+		if (this.board_.isGameOver()) {
+			return;
+		} else if (this.board_.move(x, y, player)) {
+			this.currentPlayer_ = 1 - this.currentPlayer_;
+			this.updateStatus_();
+		}
+	}
+
+	updateStatus_() {
+		const status = document.getElementById('status');
+		if (this.board_.hasWon('x')) {
+			status.innerHTML = 'Player 1 wins!';
+		} else if (this.board_.hasWon('o')) {
+			status.innerHTML = 'Player 2 wins!';
+		} else if (this.board_.isGameOver()) {
+			status.innerHTML = 'Game ended in a tie!';
+		} else {
+			status.innerHTML = 'Player ' + (this.currentPlayer_ + 1) + ' turn!';
+		}
+	}
+}
+
+new Game();
