bd14c087156a6f28bafd0843c6a313623b02c164
Sanil Shah <ss4924@columbia.edu>
2018-05-04 00:36:41 -0400

    updated time script

--- get_time.py
+++ get_time.py
@@ -6,8 +6,10 @@ def testCommand(cmd):
 	for i in range(0, 5):
 		startTime = time.time()
 		os.system(cmd)
-		totalTime += (time.time() - startTime)
-	print("Average time for: %s is %f seconds" % (cmd, totalTime/5.0))
+		runTime = time.time() - startTime
+		totalTime += runTime
+		print("##### Time for run %d: %s is %f seconds #####" % (i+1, cmd, runTime))
+	print("##### Average time for: %s is %f seconds #####" % (cmd, totalTime/5.0))
 
 testCommand('npm run test_jest')
 testCommand('npm run test_jasmine')
